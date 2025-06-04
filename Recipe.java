3ec1a8967496fcbe5db598238518d799807104ff
antonjeliazkov26 <a.jeliazkov26@acsbg.org>
2025-06-04 12:41:32 +0300

    Update Recipe.java

--- Recipe.java
+++ Recipe.java
@@ -1,39 +1,40 @@
-import java.io.Serializable;
 import java.util.List;
 
-public class Recipe implements Serializable {
-    private String name;
+public class Recipe extends RecipeItem {
     private List<String> ingredients;
     private String steps;
     private int time;
     private String difficulty;
-    private String category;
+    private String description;
 
-    public Recipe(String name, List<String> ingredients, String steps, int time, String difficulty, String category) {
-        this.name = name;
+    public Recipe(String name, List<String> ingredients, String steps, int time, String difficulty, String category, String description) {
+        super(name, category);
         this.ingredients = ingredients;
         this.steps = steps;
         this.time = time;
         this.difficulty = difficulty;
-        this.category = category;
+        this.description = description;
     }
 
-    public String getName() { return name; }
     public List<String> getIngredients() { return ingredients; }
     public String getSteps() { return steps; }
     public int getTime() { return time; }
     public String getDifficulty() { return difficulty; }
-    public String getCategory() { return category; }
+    public String getDescription() { return description; }
 
-    public void setName(String name) { this.name = name; }
     public void setIngredients(List<String> ingredients) { this.ingredients = ingredients; }
     public void setSteps(String steps) { this.steps = steps; }
     public void setTime(int time) { this.time = time; }
     public void setDifficulty(String difficulty) { this.difficulty = difficulty; }
-    public void setCategory(String category) { this.category = category; }
+    public void setDescription(String description) { this.description = description; }
+
+    @Override
+    public String getSummary() {
+        return getName() + " - " + getCategory() + " (" + time + " mins)";
+    }
 
     @Override
     public String toString() {
-        return name + " (" + category + ", " + time + " mins)";
+        return getSummary();
     }
 }
